562fc5b9e2e752cd461f46e45225b99ad5a36b9d
BloodyBear <bloody.bear.x@gmail.com>
2017-09-14 01:46:40 +0900

    Merge branch 'master' into devfarm/master
    # Conflicts:
    #	conf/evolutions/default/16.sql

--- conf/evolutions/default/16.sql
+++ conf/evolutions/default/16.sql
@@ -2,13 +2,6 @@
 ALTER TABLE n4user ADD COLUMN is_guest tinyint(1) default 0;
 CREATE INDEX ix_n4user_is_guest ON n4user (is_guest);
 
-# --- !Downs
-DROP INDEX IF EXISTS ix_n4user_is_guest ON n4user;
-ALTER TABLE n4user DROP COLUMN is_guest;
-
-# --- !Ups
-
-
 CREATE TABLE df_group
 (
 	group_sn bigint(20) NOT NULL AUTO_INCREMENT,
@@ -114,6 +107,9 @@ REFERENCES project (id)
 
 
 # --- !Downs
+
+DROP INDEX IF EXISTS ix_n4user_is_guest ON n4user;
+ALTER TABLE n4user DROP COLUMN is_guest;
 
 SET FOREIGN_KEY_CHECKS=0;
 
