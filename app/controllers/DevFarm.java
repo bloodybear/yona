e6be0cb27c58dfca14e828fd54a5c0a5c2e01526
BloodyBear <bloody.bear.x@gmail.com>
2017-09-15 22:21:11 +0900

    - 최신글 공지사항 구현

--- app/controllers/DevFarm.java
+++ app/controllers/DevFarm.java
@@ -1,11 +1,19 @@
 package controllers;
 
+import com.avaje.ebean.ExpressionList;
+import com.avaje.ebean.Page;
 import controllers.annotation.AnonymousCheck;
 import models.Organization;
+import models.Posting;
+import models.Project;
 import models.User;
+import play.data.Form;
 import play.mvc.Controller;
 import play.mvc.Result;
+import views.html.board.list;
 import views.html.devfarm.*;
+
+import java.util.List;
 
 @AnonymousCheck
 public class DevFarm extends Controller {
@@ -16,6 +24,7 @@ public class DevFarm extends Controller {
     public static String BBS_PROJECT_NAME = play.Configuration.root().getString("application.devfarm.project.bbs.name");
     public static String QNA_PROJECT_NAME = play.Configuration.root().getString("application.devfarm.project.qna.name");
     public static final int DAYS_AGO = 7;
+    public static final int ITEMS_LATEST = 10;
 
     public static Organization getOrganization() {
         return Organization.findByName(ORGANIZATION_NAME);
