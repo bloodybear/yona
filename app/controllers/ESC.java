0e1a7505fa951881ebc97cd64f37d01048c499d1
BloodyBear <bloody.bear.x@gmail.com>
2017-09-26 15:22:07 +0900

    - esc 커마

---
+++ app/controllers/ESC.java
@@ -0,0 +1,25 @@
+package controllers;
+
+import controllers.annotation.AnonymousCheck;
+import models.Organization;
+import models.User;
+import play.mvc.Controller;
+
+@AnonymousCheck(requiresLogin = true)
+public class ESC extends Controller {
+
+    public static String ORGANIZATION_NAME = play.Configuration.root().getString("esc.organization.name", "ESC");
+    public static Boolean WELCOME_SHOW = play.Configuration.root().getBoolean("esc.welcome.show", false);
+
+    public static Organization getOrganization() {
+        return Organization.findByName(ORGANIZATION_NAME);
+    }
+
+    public static boolean isAdmin(User user) {
+        return user.isAdminOf(getOrganization());
+    }
+
+    public static boolean isMember(User user) {
+        return user.isMemberOf(getOrganization());
+    }
+}
